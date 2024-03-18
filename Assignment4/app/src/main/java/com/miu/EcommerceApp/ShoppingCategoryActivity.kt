b0bc012917ad2cbb0224ea7c893556e40de5bd08
Mahmudul Hassan <93487707+MahmudHassanLalin@users.noreply.github.com>
2024-03-18 17:09:37 -0500

    Updated Assignment 4 so that toast message has been shown when category selected

--- Assignment4/app/src/main/java/com/miu/EcommerceApp/ShoppingCategoryActivity.kt
+++ Assignment4/app/src/main/java/com/miu/EcommerceApp/ShoppingCategoryActivity.kt
@@ -3,7 +3,9 @@ package com.miu.EcommerceApp
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 class ShoppingCategoryActivity : AppCompatActivity() {
@@ -14,5 +16,11 @@ class ShoppingCategoryActivity : AppCompatActivity() {
         val user = intent.extras?.getSerializable("user", User::class.java)
         val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
         tvWelcome.text = "Welcome ${user?.name}"
+    }
+
+    fun onItemClicked(view: View) {
+        val data = view.tag as String
+        val message="You have selected Category \"$data\" !!"
+        Toast.makeText(this@ShoppingCategoryActivity,message,Toast.LENGTH_SHORT).show()
     }
 }
